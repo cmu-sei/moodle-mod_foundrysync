b441239956dd88dfdeb2cf99ff129676c95675ab
Eric Bram <61522167+sei-ebram@users.noreply.github.com>
2020-10-16 12:43:19 -0400

    Set scheduled task default to */15 minutes (#2)

--- db/tasks.php
+++ db/tasks.php
@@ -5,7 +5,7 @@ $tasks = array(
     array(
         'classname' => 'tool_foundrysync\task\check_content',
         'blocking' => 0,
-        'minute' => '0',
+        'minute' => '*/15',
         'hour' => '*',
         'day' => '*',
         'dayofweek' => '*',
